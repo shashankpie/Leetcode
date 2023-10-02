1cde28820185095173b04c4a31762d06869fa07c
Shiva Shashank <shashanktech99@gmail.com>
2023-10-02 14:42:36 -0500

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -10,3 +10,28 @@ class Solution:
             if cnt > (len(nums) // 2):
                 return ele
         return -1
+
+
+# Optimal Solution - Moore’s Voting Algorithm
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 0
+        ele = nums[0]
+        for i in range(len(nums)):
+            if cnt == 0:
+                cnt = 1
+                ele = nums[i]
+            elif ele == nums[i]:
+                cnt += 1
+            else:
+                cnt -= 1
+        cnt1 = 0
+        for i in range(len(nums)):
+            if ele == nums[i]:
+                cnt1 += 1
+        if cnt1 > len(nums) // 2:
+            return ele
+        return -1
