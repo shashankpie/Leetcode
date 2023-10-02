98d605fd9c7e3fc4a5453851f49947dc8d91ea30
Shiva Shashank <shashanktech99@gmail.com>
2023-10-02 17:21:50 -0500

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -12,6 +12,24 @@ class Solution:
         return -1
 
 
+# Better Solution
+# Time complexity - O(n)
+# Space complexity - O(n)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        for num, count in hashmap.items():
+            if count > (len(nums) // 2):
+                return num
+        return -1
+
+
 # Optimal Solution - Moore’s Voting Algorithm
 # Time complexity - O(n)
 # Space complexity - O(1)
